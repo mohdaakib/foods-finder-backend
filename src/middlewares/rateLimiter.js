d955660ad6c97d514624a489ac3073e5388e734f
spidey-I2R <aakib@idea2reality.io>
2024-11-21 18:53:13 +0530

    increase rate limit

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,6 +1,6 @@
 import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 5,
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Allow 100 requests per windowMs
 });
